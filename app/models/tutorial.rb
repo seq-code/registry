18fa2ad327bb96644da194de468d2047a419b0c6
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2022-03-29 20:40:03 +0200

    Add neotype to the list of tutorials

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -47,6 +47,15 @@ class Tutorial < ApplicationRecord
                        'SeqCode or under the ICNP rules, or currently under ' \
                        'revision in the SeqCode Registry',
           steps: 10
+        },
+        neotype: {
+          title: 'Register new type',
+          prompt: 'New type for an existing taxon',
+          description: 'If you want to register a different genome as the ' \
+                       'material of an existing species or subspecies, or ' \
+                       'want to register a different genus as the type name ' \
+                       'of a taxon above the rank of genus',
+          steps: 10
         }
       }
     end
