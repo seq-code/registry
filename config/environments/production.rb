05eb436e858af0ac6cc0fcbd6741f89d454086ca
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2022-03-29 21:10:55 +0200

    Reverse to sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { host: 'http://disc-genomics.uibk.ac.at' }
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.smtp_settings = {
     address:              'smtp-relay.gmail.com',
     port:                 587,
