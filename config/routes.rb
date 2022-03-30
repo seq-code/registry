5c6f1726ac8645606616c563c12858b338abc8f2
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2022-03-30 10:30:47 +0200

    Name link fixes

--- config/routes.rb
+++ config/routes.rb
@@ -29,32 +29,32 @@ Rails.application.routes.draw do
   get  'drafts' => 'names#drafts', as: :draft_names
   get  'user-names' => 'names#user_names', as: :user_names
   get  'search' => 'application#search', as: :search
-  get  'names/batch' => 'names#batch', as: :new_name_batch
+  get  'names/batch' => 'names#batch', as: :new_batch_name
 
   resources(:names)
   resources :authors
   resources :publication_names
   get  'doi/:doi' => 'publications#show', as: :doi, doi: /.+/
   get  'publications/:id/link_names' => 'publication_names#link_names', as: :link_publication_name
-  post 'publications/:id/link_names' => 'publication_names#link_names_commit', as: :link_publication_name_commit
-  post 'names/:id/proposed_by' => 'names#proposed_by', as: :name_proposed_by
-  get  'names/:id/corrigendum_by' => 'names#corrigendum_by', as: :name_corrigendum_by
-  post 'names/:id/corrigendum' => 'names#corrigendum', as: :name_corrigendum
-  post 'names/:id/emended_by/:publication_id' => 'names#emended_by', as: :name_emended_by
+  post 'publications/:id/link_names' => 'publication_names#link_names_commit', as: :link_publication_commit_name
+  post 'names/:id/proposed_by' => 'names#proposed_by', as: :proposed_by_name
+  get  'names/:id/corrigendum_by' => 'names#corrigendum_by', as: :corrigendum_by_name
+  post 'names/:id/corrigendum' => 'names#corrigendum', as: :corrigendum_name
+  post 'names/:id/emended_by/:publication_id' => 'names#emended_by', as: :emended_by_name
   get  'names/:id/edit_etymology' => 'names#edit_etymology', as: :edit_etymology_name
   get  'names/:id/autofill_etymology' => 'names#autofill_etymology', as: :autofill_etymology
   get  'names/:id/edit_notes' => 'names#edit_notes', as: :edit_notes_name
   get  'names/:id/edit_rank' => 'names#edit_rank', as: :edit_rank_name
   get  'names/:id/edit_links' => 'names#edit_links', as: :edit_links_name
-  get  'names/:id/link_parent' => 'names#link_parent', as: :name_link_parent
+  get  'names/:id/link_parent' => 'names#link_parent', as: :link_parent_name
   get  'names/:id/type' => 'names#edit_type', as: :edit_type_name
-  post 'names/:id/link_parent' => 'names#link_parent_commit', as: :name_link_parent_commit
+  post 'names/:id/link_parent' => 'names#link_parent_commit', as: :link_parent_commit_name
   post 'names/:id/return' => 'names#return', as: :return_name
   post 'names/:id/validate' => 'names#validate', as: :validate_name
   post 'names/:id/approve' => 'names#approve', as: :approve_name
   post 'names/:id/claim' => 'names#claim', as: :claim_name
   post 'names/:id/unclaim' => 'names#unclaim', as: :unclaim_name
-  post 'names/:id/new_correspondence' => 'names#new_correspondence', as: :new_name_correspondence
+  post 'names/:id/new_correspondence' => 'names#new_correspondence', as: :new_correspondence_name
   post 'registers/:accession/submit' => 'registers#submit', as: :submit_register
   get  'registers/:accession/return' => 'registers#return', as: :return_register
   post 'registers/:accession/return' => 'registers#return_commit', as: :post_return_register
