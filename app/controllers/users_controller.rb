4171b87a1b4d0fd0c18f0f01463ad168c4120941
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2022-09-20 19:39:37 +0200

    Sort users in index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   )
 
   def index
-    @users = User.all.paginate(page: params[:page], per_page: 30)
+    @users = User.all.order('created_at').paginate(page: params[:page], per_page: 30)
   end
 
   def show
