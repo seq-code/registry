085393cae2acc59659ce9f3e1ee4831c9b4b97c9
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2022-10-14 18:04:49 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -61,7 +61,7 @@ class ApplicationController < ActionController::Base
     params[:path] = "p:#{params[:path]}" if params[:path].in? super_pages
     case params[:path]
     when *%w[robots sw favicon apple-touch-icon apple-touch-icon-precomposed]
-      path = params[:path]
+      path = params[:path].gsub(/[^A-Za-z0-9-]/, '') # So code scanner is happy
       path = "#{path}.#{params[:format]}" if params[:format]
       redirect_to(File.join(root_path, path))
     when /\Ap:(.+)\z/
