e82b54bf2a0db956d50fa5c0c3ccacc9336639c1
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2023-06-06 16:40:37 +0200

    SeqCode in-page excerpts
    Greatly improves navigability by showing SeqCode excerpts as in-page modals

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -15,15 +15,29 @@ module PageHelper
     link_to_rule_or_recommendation('recommendation', number, text)
   end
 
-  def link_to_rule_or_recommendation(section, number, text = nil)
-    if number =~ /^appendix-(\S+)$/
-      text ||= "Appendix #{$1.upcase}"
-      anchor = "appendix-#{$1}"
-    else
-      text ||= number
-      anchor = "#{section}-#{number.to_s.gsub(/\..*/, '')}"
-    end
+  def name_of_seqcode_section(section, number)
+    number = number.gsub(/-/, ' ').titleize
 
+    if section.to_s == 'rule_note'
+      "Rule #{number} (Note)"
+    else
+      "#{section.capitalize} #{number}"
+    end
+  end
+
+  def anchor_to_seqcode_section(section, number)
+    section = 'rule' if section.to_s == 'rule_note'
+
+    if number =~ /^appendix-(\S+)$/
+      "appendix-#{$1}"
+    else
+      "#{section}-#{number.to_s.gsub(/\..*/, '')}"
+    end
+  end
+
+  def link_to_rule_or_recommendation(section, number, text = nil)
+    anchor = anchor_to_seqcode_section(section, number)
+    text ||= name_of_seqcode_section(section, number)
     link_to_seqcode_section(text, anchor)
   end
 
@@ -31,5 +45,45 @@ module PageHelper
     link_to(page_seqcode_path(anchor: anchor)) do
       content_tag(:u, text)
     end
+  end
+
+  def seqcode_excerpt(section, number)
+    unless @sq_noko
+      @sq_down  ||= Redcarpet::Render::HTML.new(with_toc_data: true)
+      @markdown ||= Redcarpet::Markdown.new(@sq_down, autolink: true, tables: true)
+      @seqcode  ||= @markdown.render(File.read(Rails.root.join('seqcode', 'README.md')))
+      @sq_noko = Nokogiri::HTML(@seqcode)
+      # Fix in-page links
+      @sq_noko.xpath("//*[contains(@href, '#')]").each do |i|
+        i[:href] = "#{page_seqcode_path}#{i[:href]}" if i[:href] =~ /^#/
+      end
+    end
+    
+    id = anchor_to_seqcode_section(section, number)
+    sel = "*[@id='#{id}']"
+
+    content = @sq_noko.xpath("//#{sel}/following-sibling::*")
+                      .take_while { |i| !i['id'] }
+    if number =~ /\.(\d+)/
+      k = $1.to_i
+      content = content.map do |i|
+        i.dup.tap { |y| y.xpath("li[#{k}]").set(:style, 'background:#ffeeaa;') }
+      end
+    end
+    @sq_noko.xpath("//#{sel}[1]").to_s.html_safe +
+        content.map(&:to_s).join.html_safe
+  end
+
+  def link_to_seqcode_excerpt(section, number, text = nil)
+    anchor = anchor_to_seqcode_section(section, number)
+    text ||= name_of_seqcode_section(section, number)
+
+    id = modal('SeqCode Excerpt', size: 'lg') do
+      content_tag(:div, seqcode_excerpt(section, number), class: 'px-4 py-2') +
+        content_tag(:hr) +
+        fa_icon('hand-point-right', class: 'ml-4 mr-2') +
+        link_to_rule_or_recommendation(section, number, 'See in context')
+    end
+    modal_button(id, class: '', type: '', tag: :a, href: '#') { text }
   end
 end
