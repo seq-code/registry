dee0f8e5a6b7b5616be2e31fe2df82bbf91ae6e4
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2024-01-10 21:50:14 +0100

    Additional culture collections

--- app/models/name.rb
+++ app/models/name.rb
@@ -255,24 +255,40 @@ class Name < ApplicationRecord
               'NBRCCatalogueDetailServlet?ID=IFO&CAT=%s',
         NCTC: 'https://www.culturecollections.org.uk/products/bacteria/' \
               'detail.jsp?collection=nctc&refId=NCTC+%s',
+        # Collection de l'Institut Pasteur
+        # [FR: Institut Pasteur]
         CIP:  'https://catalogue-crbip.pasteur.fr/' \
               'fiche_catalogue.xhtml?crbip=CIP%%20%s',
+        # Pasteur Cultures of Cyanobacteria
+        # [FR: Institut Pasteur]
         PCC:  'https://catalogue-crbip.pasteur.fr/' \
               'fiche_catalogue.xhtml?crbip=PCC%%20%s',
+        # Culture Collection University of Gothenburg
+        # [SE: University of Gothenburg]
         CCUG: 'https://www.ccug.se/strain?id=%s',
+        # Agricultural Research Service Culture Collection,
+        # formerly Northern Regional Research Laboratory
+        # [US: National Center for Agricultural Utilization Research]
         NRRL: 'https://nrrl.ncaur.usda.gov/cgi-bin/usda/prokaryote/' \
               'report.html?nrrlcodes=%s',
         # Collection of Aquatic Important Microorganisms
-        # [Mexico: CIAD, Centro de Investigación en Alimentación y Desarrollo]
+        # [MX: CIAD, Centro de Investigación en Alimentación y Desarrollo]
         CAIM: 'https://www.ciad.mx/caim/busqueda.php?' \
               'searchtype=caim&criterio=%s',
         # NCMA, National Center for Marine Algae and Microbiota,
         # formerly Culture Collection for Marine Phytoplankton
-        # [USA: Bigelow Laboratory for Ocean Sciences]
+        # [US: Bigelow Laboratory for Ocean Sciences]
         CCMP: 'https://ncma.bigelow.org/CCMP%s',
         # North East Pacific Culture Collection
-        # [Canada: The University of British Columbia]
-        NEPCC: 'https://db.botany.ubc.ca/cccm/mfa/%s'
+        # [CA: The University of British Columbia]
+        NEPCC: 'https://db.botany.ubc.ca/cccm/mfa/%s',
+        # Marine Culture Collection of China
+        # [CN: Third Institute of Oceanography]
+        MCCC: 'https://mccc.org.cn/detailRecord3.asp?bcbh=%s',
+        # Korean Agricultural Culture Collection
+        # [KR: National Academy of Agricultural Science]
+        KACC: 'https://genebank.rda.go.kr/eng/mic/cat/MicrobeSearch.do' \
+              '?sSearchWith=no&sTxt1=%s'
       }
     end
   end
