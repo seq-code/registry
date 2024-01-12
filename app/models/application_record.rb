3ad7c5e385e7e0fa7959d02cf730bb8b016fed19
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2024-01-12 02:48:06 +0100

    Update application_record.rb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  # has_many(:notified_notifications, as: :linkeable, dependent: :destroy)
-  # has_many(:linked_notifications, as: :linkeable, dependent: :nullify)
+  has_many(
+    :notified_notifications, class_name: 'Notification',
+    as: :notifiable, dependent: :destroy
+  )
+  has_many(:linked_notifications, class_name: 'Notification',
+    as: :linkeable, dependent: :nullify
+  )
 end
