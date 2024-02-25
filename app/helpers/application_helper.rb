5b9d47c852846b0581e90201eea473ef5f77a082
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2024-02-25 15:49:28 +0100

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,7 @@ module ApplicationHelper
 
   def time_ago_with_date(date)
     date  = DateTime.parse(date) if date.is_a?(String)
-    fdate = date.strftime('%F %r (%Z)')
+    fdate = date.strftime('%F %I:%M %p (%Z)')
     content_tag(:u, class: 'hover-help', title: fdate) do
       time_ago_in_words(date) + ' ago'
     end
