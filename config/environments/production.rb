7ebe28aa6cb6ae5972b63f68958894a8152daf64
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2024-05-03 21:54:44 +0200

    Fix mail host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'http://disc-genomics.uibk.ac.at' }
+  config.action_mailer.default_url_options = { host: 'https://registry.seqco.de' }
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.smtp_settings = {
     address:              'smtp-relay.gmail.com',
