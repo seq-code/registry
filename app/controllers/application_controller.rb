53f408d2e30ffca4e89b0c5190c79b6a26963cc5
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2024-05-10 16:29:00 +0200

    Remember location after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery(with: :exception)
   before_action(:check_api!)
+  before_action(:store_user_location!, if: :storable_location?)
 
   @@search_obj = {
     publications: [Publication, %w[title doi journal abstract], {}],
@@ -167,5 +168,15 @@ class ApplicationController < ActionController::Base
         end
       end
     end
+
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? &&
+        !request.xhr?
+    end
+
+    def store_user_location!
+      store_location_for(:user, request.fullpath)
+    end
+
 
 end
