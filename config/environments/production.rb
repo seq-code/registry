e4d5d71c40718d88f2fb2bcc0045b741247e5ad8
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2024-08-03 10:59:25 +0200

    Compress JS (and CSS)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,10 @@ Rails.application.configure do
   config.action_controller.relative_url_root = ENV['RAILS_RELATIVE_URL_ROOT']
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
+
+  # Compress JS using a preprocesso
+  config.assets.js_compressor = :uglifier
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
