973ad738600521601246e84a67a615095cc3e583
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2024-11-24 22:20:56 +0300

    Update names_controller.rb

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -238,7 +238,7 @@ class NamesController < ApplicationController
   # GET /names/1/wiki
   def wiki
     @crumbs = [['Names', names_path], [@name.name_html, @name], 'Wiki source']
-    @name.check_wikispecies if current_curator? # Force re-check
+    @name.check_wikispecies if current_user.try(:curator?) # Force re-check
   end
 
   # GET /names/new
