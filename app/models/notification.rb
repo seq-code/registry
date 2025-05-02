0f971e48b233b2147f31ccc8d0ef340b6b5cad88
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2025-05-02 13:33:02 +0200

    Turn off Action Cable calls for now

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,8 +4,8 @@ class Notification < ApplicationRecord
   belongs_to :linkeable, polymorphic: true, optional: true
 
   after_create(:send_email_notification)
-  after_create(:broadcast_web_notification)
-  after_save(:broadcast_notification_count)
+  ### after_create(:broadcast_web_notification)
+  ### after_save(:broadcast_notification_count)
 
   private
 
