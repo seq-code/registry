517bf3c6db535f6c628ce9924502b967154e9939
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2025-05-16 18:24:59 +0200

    Toast support

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -1,6 +1,7 @@
 
-// This function depends upon the Bootstrap application, so it cannot
+// These functions depend upon the Bootstrap application, so they cannot
 // currently be shipped as a pack
+
 $(document).on("turbolinks:load", function() {
   $(".modal[data-async]").each(function() {
     var container = $(this);
@@ -14,4 +15,27 @@ $(document).on("turbolinks:load", function() {
     });
   });
 });
+
+function new_toast(title, body, url) {
+  var toast = $(
+    "<div class=toast role=alert aria-live=assertive aria-atomic=true " +
+      "data-autohide=false data-delay=30000></div>"
+  );
+  toast.append($(
+    "<div class=toast-header>" +
+      "<strong class=mr-auto>" + title + "</strong>" +
+      "<button type=button class='ml-2 mb-1 close' data-dismiss='toast' " +
+        "aria-label=Close><span aria-hidden=true>&times;</span>" +
+      "</button>" +
+    "</div>"
+  ));
+  var toast_body = $("<div class='toast-body'></div>")
+      .append(
+        $("<a class='btn btn-sm text-left' href='" + url + "'></a>")
+          .append(body)
+      );
+  toast.append(toast_body);
+  $("#toastCont").append(toast);
+  toast.toast('show');
+}
 
