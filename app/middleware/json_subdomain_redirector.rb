a00f655b7277e5fe9b670df3fdebfc2992dc8662
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2025-07-31 11:23:07 +0200

    Redirect all JSON requests to API

---
+++ app/middleware/json_subdomain_redirector.rb
@@ -0,0 +1,38 @@
+class JsonSubdomainRedirector
+  def initialize(app)
+    @api_domain = 'api.seqco.de'
+    @api_version = 'v1'
+    @app = app
+  end
+
+  def call(env)
+    request = Rack::Request.new(env)
+
+    if is_json?(request) && !already_on_api_subdomain?(request)
+      is_versioned = request.path.match?(/^\/v\d+\//)
+      new_path = is_versioned ? request.path : "/#{@api_version}#{request.path}"
+
+      # Reconstruct URL
+      uri = URI.parse(request.url)
+      uri.host = @api_domain
+      uri.path = new_path
+
+      return [
+        301,
+        { 'Location' => uri.to_s, 'Content-Type' => 'text/html' },
+        ['Moved Permanently']
+      ]
+    end
+
+    @app.call(env)
+  end
+
+  def is_json?(request)
+    request.get_header('HTTP_ACCEPT')&.include?('application/json') ||
+      request.path.end_with?('.json')
+  end
+
+  def already_on_api_subdomain?(request)
+    request.host == @api_domain
+  end
+end
