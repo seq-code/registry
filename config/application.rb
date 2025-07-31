3e042cf8e46c34ff37c70898bf7194a46a45345b
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2025-07-31 11:41:49 +0200

    Manually load middleware

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,11 @@ module CandidatusExcubia
     config.middleware.use ActionDispatch::Flash
 
     config.autoload_paths << Rails.root.join('lib')
-    config.autoload_paths << Rails.root.join('app/middleware')
     config.eager_load_paths << Rails.root.join('lib')
-    config.eager_load_paths << Rails.root.join('app/middleware')
+
+    if Rails.env.production?
+      require Rails.root.join('app/middleware/json_subdomain_redirector')
+      config.middleware.use ::JsonSubdomainRedirector
+    end
   end
 end
