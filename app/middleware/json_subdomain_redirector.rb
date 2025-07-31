9d7edf3945dca4ed9b949f85c9f65678400927ca
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2025-07-31 11:54:03 +0200

    Update json_subdomain_redirector.rb

--- app/middleware/json_subdomain_redirector.rb
+++ app/middleware/json_subdomain_redirector.rb
@@ -36,7 +36,8 @@ class JsonSubdomainRedirector
   end
 
   def is_core_json?(request)
-    request.path.end_with?('autocomplete.json')
+    request.path.end_with?('autocomplete.json') ||
+      request.path.end_with?('network.json')
   end
 
   def already_on_api_subdomain?(request)
