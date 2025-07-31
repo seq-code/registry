fbf7341e8f864de648c6efc94f615d8bbe779a1f
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2025-07-31 11:51:24 +0200

    Update json_subdomain_redirector.rb

--- app/middleware/json_subdomain_redirector.rb
+++ app/middleware/json_subdomain_redirector.rb
@@ -10,28 +10,33 @@ class JsonSubdomainRedirector
   def call(env)
     request = Rack::Request.new(env)
 
-    if is_json?(request) && !already_on_api_subdomain?(request)
-      is_versioned = request.path.match?(/^\/v\d+\//)
-      new_path = is_versioned ? request.path : "/#{@api_version}#{request.path}"
+    return @app.call(env) if
+      !is_json?(request) ||
+      is_core_json?(request) ||
+      already_on_api_subdomain?(request)
 
-      # Reconstruct URL
-      uri = URI.parse(request.url)
-      uri.host = @api_domain
-      uri.path = new_path
+    is_versioned = request.path.match?(/^\/v\d+\//)
+    new_path = is_versioned ? request.path : "/#{@api_version}#{request.path}"
 
-      return [
-        301,
-        { 'Location' => uri.to_s, 'Content-Type' => 'text/html' },
-        ['Moved Permanently']
-      ]
-    end
+    # Reconstruct URL
+    uri = URI.parse(request.url)
+    uri.host = @api_domain
+    uri.path = new_path
 
-    @app.call(env)
+    return [
+      301,
+      { 'Location' => uri.to_s, 'Content-Type' => 'text/html' },
+      ['Moved Permanently']
+    ]
   end
 
   def is_json?(request)
     request.get_header('HTTP_ACCEPT')&.include?('application/json') ||
       request.path.end_with?('.json')
+  end
+
+  def is_core_json?(request)
+    request.path.end_with?('autocomplete.json')
   end
 
   def already_on_api_subdomain?(request)
