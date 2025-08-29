09ca744e5b47c306cd371aa0aecda51eb0d1d4ac
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2025-08-29 13:19:28 +0200

    New JS/CSS class `copyable-block`

--- app/javascript/packs/copyable.js
+++ app/javascript/packs/copyable.js
@@ -1,15 +1,21 @@
 $(document).on("turbolinks:load", function() {
   $(".copyable:not(:has(.copyer))").each(function() {
     var elem = $(this);
-    var cont = elem.text().replace(/\s+/gm, " ");
+    var cont = elem.text()
     var copied = $(
       '<span style="display: none;" class="small">' +
       'Copied to the clipboard</span>'
     );
     var btn = $(
-      '<a class="badge badge-pill badge-primary mx-2 copyer" type="button">' +
+      '<a class="badge badge-pill badge-primary mr-2 copyer" type="button">' +
       '<i class="fas fa-copy mr-1"> </i> Copy</a>'
     );
+    if (elem.hasClass("copyable-block")) {
+      btn.addClass("mt-2");
+    } else {
+      btn.addClass("ml-2");
+      cont = cont.replace(/\s+/gm, " ");
+    }
     btn.on("click", function() {
       navigator.clipboard.writeText(cont);
       copied.fadeIn(200).delay(800).fadeOut(800);
