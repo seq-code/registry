cf888c517ffff6bb47eca792faf2beea99c3bf07
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2025-10-14 15:40:43 +0200

    Correct POST form

--- app/models/register_coauthor.rb
+++ app/models/register_coauthor.rb
@@ -9,10 +9,21 @@ class RegisterCoauthor < ApplicationRecord
 
   after_destroy(:update_coauthors_order)
 
+  ##
+  # Is this the first coauthor? Note that this means this is the
+  # second author of the list (since the submitting user is always
+  # first in the list of authors)
+  def first?
+    order == 1
+  end
+
+  ##
+  # Move the coauthor one position up in the order of coauthors
   def move_up
     return if order <= 1
 
-    register.register_coauthor.where(order: order - 1)
+    register
+      .register_coauthor.where(order: order - 1)
       .update(order: order) && update(order: order - 1)
   end
 
