867192a5068647453d3173c80e7a3a5c0107eed9
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2025-11-06 10:52:53 +0100

    Standardize author names

--- app/models/author.rb
+++ app/models/author.rb
@@ -7,9 +7,24 @@ class Author < ApplicationRecord
 
   class << self
     def find_or_create(given, family)
-      par = { given: given, family: family }
+      par = {
+        given: standardize_given(given),
+        family: standardize_family(family)
+      }
       author = where(par).first
       author ||= Author.new(par).tap(&:save)
+    end
+
+    def standardize_given(given)
+      if given.to_s.match(/\A[A-Z]( [A-Z])* ?\z/)
+        given.to_s.gsub(/( | ?$)/, '. ').gsub(/ $/, '')
+      else
+        standardize_family(given)
+      end
+    end
+
+    def standardize_family(family)
+      family == family.to_s.upcase ? family.titlecase : family
     end
   end
 
@@ -19,5 +34,10 @@ class Author < ApplicationRecord
 
   def abbreviated_name
     family.to_s + ( given ? ' ' + given.gsub(/([^\s])[^\s]+ ?/, '\1') : '' )
+  end
+
+  def standard_name?
+    family == self.class.standardized_family(family) &&
+      given == self.class.standardized_given(given)
   end
 end
---
+++ lib/tasks/clean_authors.rake
@@ -0,0 +1,30 @@
+namespace :authors do
+  desc 'Standardize author names, find duplicates, and merge them'
+  task :clean => :environment do |t, args|
+    km = 0
+    ks = 0
+    Author.each do |author|
+      next if author.standard_name?
+
+      par = {
+        family: Author.standardize_family(author.family),
+        given: Author.standardize_given(author.given)
+      }
+
+      if s_author = Author.find_by(par)
+        puts '~ Merging %s -> %s' % [author.full_name, s_author.full_name]
+        # Replace all associations
+        author.publication_authors.update(author: s_author)
+        author.contacts.update(author: s_author)
+        # And remove
+        author.destroy
+        km += 1
+      else
+        puts '~ Standardizing %s' % [author.full_name]
+        author.update(par)
+        ks += 1
+      end
+    end
+    puts 'Merged %i authors and standardized %s names' % [km, ks]
+  end
+end
