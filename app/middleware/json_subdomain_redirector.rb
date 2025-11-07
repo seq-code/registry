131cf73df856604a745b9b1fb7ae41615b8257e2
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2025-11-07 13:21:31 +0100

    Refactor JSON subdomain redirector logic
    Reworks the JsonSubdomainRedirector to explicitly redirect API calls to the API domain and GUI calls to the GUI domain. Introduces clearer separation of redirect logic and improves method naming for better readability and maintainability.

--- app/middleware/json_subdomain_redirector.rb
+++ app/middleware/json_subdomain_redirector.rb
@@ -1,7 +1,11 @@
 require 'uri'
 
+##
+# Make sure that the API calls are channeled through the API domain and the
+# GUI calls are channeled through the GUI domain
 class JsonSubdomainRedirector
   def initialize(app)
+    @gui_domain = 'registry.seqco.de'
     @api_domain = 'api.seqco.de'
     @api_version = 'v1'
     @app = app
@@ -10,17 +14,35 @@ class JsonSubdomainRedirector
   def call(env)
     request = Rack::Request.new(env)
 
-    return @app.call(env) if
-      !is_json?(request) ||
-      is_core_json?(request) ||
-      already_on_api_subdomain?(request)
+    if on_api_subdomain?(request)
+      redirect_to_gui(request) unless is_api_call?(request)
+    else
+      redirect_to_api(request) if is_api_call?(request)
+    end
+  end
 
+  def redirect_to_api(request)
     is_versioned = request.path.match?(/^\/v\d+\//)
     new_path = is_versioned ? request.path : "/#{@api_version}#{request.path}"
 
     # Reconstruct URL
     uri = URI.parse(request.url)
     uri.host = @api_domain
+    uri.path = new_path
+
+    return [
+      301,
+      { 'Location' => uri.to_s, 'Content-Type' => 'text/html' },
+      ['Moved Permanently']
+    ]
+  end
+
+  def redirect_to_gui(request)
+    new_path = request.path.gsub(/^\/v\d+\//, '/')
+
+    # Reconstruct URL
+    uri = URI.parse(request.url)
+    uri.host = @gui_domain
     uri.path = new_path
 
     return [
@@ -42,7 +64,11 @@ class JsonSubdomainRedirector
       path.match?(/check\/\d+\.json$/)
   end
 
-  def already_on_api_subdomain?(request)
+  def is_api_call?(request)
+    is_json?(request) && !is_core_json?(request)
+  end
+
+  def on_api_subdomain?(request)
     request.host == @api_domain
   end
 end
