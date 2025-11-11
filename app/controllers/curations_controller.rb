0f2cf6bc5ef115226eaec43fb1ce9995ff056891
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2025-11-11 15:10:56 +0100

    Live curation tracking

---
+++ app/controllers/curations_controller.rb
@@ -0,0 +1,39 @@
+class CurationsController < ApplicationController
+  before_action :authenticate_curator!
+  before_action :set_curation, only: %i[update]
+
+  # POST /curations
+  def create
+    @curation = Curation.new(curation_params)
+
+    if @curation.save
+      flash[:notice] = 'Curation tracking registered'
+    else
+      flash.now[:danger] = 'Curation tracking could not be registered'
+    end
+    redirect_back(fallback_location: curation.name&.register)
+  end
+
+  # POST /curations/1
+  def update
+    if @curation.update(curation_params)
+      flash[:notice] = 'Curation tracking registered'
+    else
+      flash.now[:danger] = 'Curation tracking could not be registered'
+    end
+    redirect_back(fallback_location: curation.name&.register)
+  end
+
+  private
+    def set_curation
+      @curation = Curation.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def curation_params
+      params
+        .require(:curation)
+        .permit(:name_id, :status_int, :notes)
+        .merge(user: current_user)
+    end
+end
