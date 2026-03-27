229a0378d4c130585e10ba08fc499d53a975624a
Luis M Rodriguez-R <miguel@rodriguez-r.com>
2026-03-27 18:26:10 +0100

    First attempt at sitemaps

---
+++ config/sitemap.rb
@@ -0,0 +1,95 @@
+SitemapGenerator::Sitemap.default_host = 'https://registry.seqco.de'
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+
+# Top-level pages
+SitemapGenerator::Sitemap.create do
+  extend HelpTopics
+  add root_path, changefreq: :daily, priority: 1.0
+
+  # Pages
+  add page_api_path, changefreq: :monthly, priority: 0.4
+  add page_about_path, changefreq: :weekly, priority: 0.8
+  add page_committee_path, changefreq: :monthly, priority: 0.6
+  add page_news_path, changefreq: :monthly, priority: 0.6
+  add page_prize_path, changefreq: :monthly, priority: 0.4
+  add page_publications_path, changefreq: :monthly, priority: 0.4
+  add page_seqcode_path, changefreq: :monthly, priority: 0.8
+
+  # Help
+  add help_index_path, changefreq: :weekly, priority: 0.8
+  help_topics.each do |_cat, topics|
+    topics.each do |topic, _data|
+      add help_path(topic: topic), changefreq: :monthly, priority: 0.8
+    end
+  end
+
+  # User registration
+  add '/sign_up', changefreq: :monthly, priority: 0.5
+  add '/login', changefreq: :monthly, priority: 0.5
+
+  # Indexes
+  add name_type_genomes_path, changefreq: :weekly, priority: 0.6
+  add names_path, changefreq: :daily, priority: 0.8
+  add submitted_names_path, changefreq: :daily, priority: 0.8
+  add endorsed_names_path, changefreq: :daily, priority: 0.8
+  add strains_path, changefreq: :daily, priority: 0.6
+  add registers_path, changefreq: :weekly, priority: 0.8
+  add authors_path, changefreq: :daily, priority: 0.4
+  add journals_path, changefreq: :monthly, priority: 0.4
+  add publications_path, changefreq: :daily, priority: 0.4
+  add subjects_path, changefreq: :monthly, priority: 0.2
+  add search_path, changefreq: :monthly, priority: 0.2
+end
+
+# Names
+SitemapGenerator::Sitemap.namer = SitemapGenerator::SimpleNamer.new(:names)
+SitemapGenerator::Sitemap.create do
+  Name.all_public.find_each do |name|
+    add name_path(name), lastmod: name.updated_at,
+        changefreq: :weekly, priority: 0.6
+    # TODO:
+    # - add wiki
+    # - add network
+  end
+end
+
+# Genomes
+SitemapGenerator::Sitemap.namer = SitemapGenerator::SimpleNamer.new(:genomes)
+SitemapGenerator::Sitemap.create do
+  Genome.all_public.find_each do |genome|
+    add genome_path(genome), lastmod: genome.updated_at,
+        changefreq: :weekly, priority: 0.4
+    # TODO:
+    # - add sample_map
+  end
+end
+
+# Strains
+SitemapGenerator::Sitemap.namer = SitemapGenerator::SimpleNamer.new(:strains)
+SitemapGenerator::Sitemap.create do
+  Strain.find_each do |strain|
+    add strain_path(strain), lastmod: strain.updated_at,
+        changefreq: :weekly, priority: 0.4
+  end
+end
+
+# Registers
+SitemapGenerator::Sitemap.namer = SitemapGenerator::SimpleNamer.new(:registers)
+SitemapGenerator::Sitemap.create do
+  Register.where(validated: true).find_each do |register|
+    add register_path(register), lastmod: register.updated_at,
+        changefreq: :weekly, priority: 0.6
+    # TODO:
+    # - add table
+    # - add list
+    # - add sample_map
+    # - add tree
+  end
+end
+
+# TODO Add members for:
+# - authors
+# - journals
+# - publications
+# - subjects
+
