ce45c8b056e1b2a317ff0094a463bb0ad62dbb4d
Luis M Rodriguez-R <miguel@rodriguez-r.com>
2026-04-01 12:05:18 +0200

    Update rack_attack.rb

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,5 +1,5 @@
 class Rack::Attack
-  throttle('req/ip', limit: 100, period: 1.minute) do |req|
+  throttle('req/ip', limit: 20, period: 1.minute) do |req|
     req.ip if req.path == '/publications'
   end
 end
