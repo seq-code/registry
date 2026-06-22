3c030e64535858f7f652c56988a49c50cab648fc
Luis M Rodriguez-R <miguel@rodriguez-r.com>
2026-06-22 11:14:21 +0200

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module CandidatusExcubia
 
     config.autoload_paths << Rails.root.join('lib')
     config.eager_load_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('app/services')
 
     if Rails.env.production?
       require Rails.root.join('app/middleware/json_subdomain_redirector')
