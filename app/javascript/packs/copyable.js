18d7c837b81b73d55efe24baa7b5c15f7be8d489
Luis M Rodriguez-R <miguel@rodriguez-r.com>
2026-07-27 12:15:09 +0200

    Improve copyable style

--- app/javascript/packs/copyable.js
+++ app/javascript/packs/copyable.js
@@ -3,25 +3,36 @@ $(document).on("turbolinks:load", function() {
     var elem = $(this);
     var cont = elem.text()
     var copied = $(
-      '<span style="display: none;" class="small">' +
-      'Copied to the clipboard</span>'
+      '<span class="mr-2 font-weight-lighter"' +
+        ' style="display: none;">copied</span>'
     );
     var btn = $(
-      '<a class="badge badge-pill badge-primary mr-2 copyer" type="button">' +
-      '<i class="fas fa-copy mr-1"> </i> Copy</a>'
+      '<a class="badge badge-pill badge-primary copyer float-right pl-2"' +
+        ' type="button"><i class="fas fa-copy mr-1"> </i></a>'
     );
     if (elem.hasClass("copyable-block")) {
       btn.addClass("mt-3");
     } else {
-      btn.addClass("ml-2");
+      btn.addClass("mt-1 ml-2");
       cont = cont.replace(/\s+/gm, " ");
     }
     btn.on("click", function() {
       navigator.clipboard.writeText(cont);
-      copied.fadeIn(200).delay(800).fadeOut(800);
+      var i = btn.find("i.fas");
+      i.slideUp(200).slideDown(200).delay(2000).slideUp(200).slideDown(200);
+      setTimeout(() => {
+        i.addClass("fa-check").removeClass("fa-copy");
+        copied.slideDown(200);
+      }, 200);
+      setTimeout(() => {
+        copied.slideUp(200);
+      }, 2400);
+      setTimeout(() => {
+        i.addClass("fa-copy").removeClass("fa-check");
+      }, 2600);
     });
+    btn.prepend(copied);
     elem.append(btn);
-    elem.append(copied);
   });
 });
 
